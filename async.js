563e07ff352241f7aff0168265f4a20c7a73bcce
mistymidnights <mistydeveloping@gmail.com>
2022-07-23 20:31:26 +0200

    save

--- async.js
+++ async.js
@@ -40,3 +40,31 @@ boton.onclick = clickEvent;
 //1.4 En base al ejercicio anterior, crea un botón con el texto 'X' para cada uno 
 //de los p que hayas insertado y que si el usuario hace click en este botón 
 //eliminemos el parrafo asociado.
+
+
+
+
+/* Iteración #2: async-await */
+//2.1 Convierte la siguiente promesa para esperar a ejecutar el console usando 
+//async-await.
+
+/* const runTimeOut = async () => {
+    const promise = await new Promise((resolve) => {
+        setTimeout(function () {
+            resolve();
+        }, 2000);
+    })
+
+    promise.then(() => {console.log('Time out!')})
+};
+
+runTimeOut(); */
+
+//2.2 Convierte la siguiente función con un fetch utilizando async-await. 
+//Recuerda que para usar .fetch() tendrás que probar el ejercicio en el navegador;
+
+/* const getCharacters2 = async () => {
+    fetch('https://rickandmortyapi.com/api/character').then(res => res.json()).then(characters => console.log(characters));
+}
+
+getCharacters(); */
